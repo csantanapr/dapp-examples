a23535e8e6b8d755987f175c4e9e06893a88fbe2
Carlos Santana <csantana23@gmail.com>
2013-07-30 09:13:02 -0400

    Initial website

---
+++ dapp-request/dist/www/app/nls/main_tr.js
@@ -0,0 +1,5 @@
+define('app/nls/main_tr',{
+'app/nls/app_strings':{"hello":"hello","world":"world","app_name":"Request App","my_requests":"My Rquests","back":"Back"}
+,
+'app/views/list/nls/list-strings':{"search":"Search/Sort","create":"Create","add":"Add"}
+});
