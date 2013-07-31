74f4991f82b5f756d35c3afdd5811b20b8ed549e
Carlos Santana <csantana23@gmail.com>
2013-07-31 17:38:01 -0400

    updated dist with details stub

--- dapp-request/dist/www/app/nls/main_en-gb.js
+++ dapp-request/dist/www/app/nls/main_en-gb.js
@@ -1,5 +1,5 @@
 define('app/nls/main_en-gb',{
-'app/nls/app_strings':{"hello":"hello","world":"world","app_name":"Request App","my_requests":"My Requests","back":"Back"}
+'app/nls/app_strings':{"hello":"hello","world":"world","app_name":"Request App","my_requests":"My Requests","back":"Back","request_details":"Request Details"}
 ,
 'app/views/list/nls/list-strings':{"search":"Search/Sort","create":"Create","add":"Add"}
 });
--- dapp-request/dist/www/app/nls/main_es.js
+++ dapp-request/dist/www/app/nls/main_es.js
@@ -1,5 +1,5 @@
 define('app/nls/main_es',{
-'app/nls/app_strings':{"hello":"hola","world":"mundo","app_name":"Pedidos","my_requests":"Mis Pedidos","back":"Atras"}
+'app/nls/app_strings':{"hello":"hola","world":"mundo","app_name":"Pedidos","my_requests":"Mis Pedidos","back":"Atras","request_details":"Detalles del Pedido"}
 ,
 'app/views/list/nls/list-strings':{"search":"Busca","create":"Crea","add":"Añade"}
 });
