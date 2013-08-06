173ff35f80f90ded897d214106f19eb0ffdd7dad
Carlos Santana <csantana23@gmail.com>
2013-08-06 15:18:36 -0400

    updated gitignore

--- dapp-request/dist/cordova/dApp/www/app/nls/main_zh-cn.js
+++
@@ -1,7 +0,0 @@
-define('app/nls/main_zh-cn',{
-'app/nls/app_strings':{"hello":"hello","world":"world","app_name":"Request App","my_requests":"My Requests","back":"Back","cancel":"Cancel","requests":"Requests","list":"List"}
-,
-'app/views/list/nls/list-strings':{"search":"Search/Sort","create":"Create","add":"Add"}
-,
-'app/views/details/nls/details-strings':{"id":"Id","requestType":"Request Type","description":"Description","status":"Status","priority":"Priority","requestedBy":"Requested By","requestedFinishDate":"Requested Finish Date","assignedTo":"Assigned To","actualFinishDate":"Actual Finish Date","estimatedUnits":"Estimated Units","unitType":"Unit Type","createdDate":"Created Date","save":"Save","copy":"Copy","updatedDate":"Updated Date","home":"Home","work":"Work","cell":"Cell","name":"Name","first":"Firstname","last":"Lastname","company":"Company","details":"Details","mail":"E-mail","phone":"Phone","edit":"Edit","cancel":"Cancel","ok":"Ok","remove":"Delete","request_details":"Request Details"}
-});
